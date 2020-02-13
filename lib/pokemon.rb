f5c6a65950963b9fa1057109199bb90fccd53c0c
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2020-02-13 22:04:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,21 @@
 require 'pry'
 class Pokemon
+<<<<<<< HEAD
   attr_accessor :name, :type, :id, :db 
   
   def initialize(id:, name:, type:, db:)
+=======
+  attr_accessor :name, :type, :id
+  attr_reader :db 
+  
+  def initialize(id = nil , name, type, db = nil)
+>>>>>>> a784b1dcddf99c483b6afc80c3b050c6614a289e
     @id = id 
     @name = name
     @type = type 
     @db = db 
   end 
+<<<<<<< HEAD
   
   def self.save(name, type, db)
     sql = <<-SQL
@@ -23,4 +31,6 @@ class Pokemon
   end
   
   def self.find(num, )
+=======
+>>>>>>> a784b1dcddf99c483b6afc80c3b050c6614a289e
 end
